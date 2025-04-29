d79b1c8c5db6d3f5aefe787baf4a7bc80878a5da
Younghwan Mun <ymun@rocketsoftware.com>
2025-04-29 11:58:24 -0400

    implement thread local storage

--- src/core/app.c
+++ src/core/app.c
@@ -1,6 +1,9 @@
 #include <config.h>
 #include <KD/kd.h>
+#include <KD/KHR_thread_storage.h>
 #include <stdlib.h>
+
+KDThreadStorageKeyKHR tls_key;
 
 KD_API KD_NORETURN void KD_APIENTRY kdExit(KDint status)
 {
@@ -9,9 +12,11 @@ KD_API KD_NORETURN void KD_APIENTRY kdExit(KDint status)
 
 KD_API KDint KD_APIENTRY kdInit(int argc, char *argv[])
 {
+    (void) kdCreateThreadStorageKHR(&tls_key, KD_NULL);
     return 0;
 }
 
 KD_API void KD_APIENTRY kdDeinit(void)
 {
+    kdDeleteThreadStorageKHR(tls_key);
 }
--- src/core/tls.c
+++ src/core/tls.c
@@ -1,11 +1,15 @@
 #include <config.h>
 #include <KD/kd.h>
+#include <KD/KHR_thread_storage.h>
+
+extern KDThreadStorageKeyKHR tls_key;
 
 KD_API void *KD_APIENTRY kdGetTLS(void)
 {
-    return KD_NULL;
+    return kdGetThreadStorageKHR(tls_key);
 }
 
 KD_API void KD_APIENTRY kdSetTLS(void *ptr)
 {
+    (void) kdSetThreadStorageKHR(tls_key, ptr);
 }
