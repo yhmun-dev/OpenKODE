079b8fcfd5f2c39fdc90044a4dafcb898911846b
Younghwan Mun <yhmun0409@gmail.com>
2025-04-29 19:48:27 -0400

    implement assertion and logging function

--- src/core/assert.c
+++ src/core/assert.c
@@ -1,10 +1,38 @@
 #include <config.h>
 #include <KD/kd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 KD_API void KD_APIENTRY kdHandleAssertion(const KDchar *condition, const KDchar *filename, KDint linenumber)
 {
+    fprintf(stderr, "*** Assertion Failure ***");
+    fprintf(stderr, " Condition  : %s", condition);
+    fprintf(stderr, " File Name  : %s", filename);
+    fprintf(stderr, " Line Number: %d", linenumber);
+
+    kdExit(3);
 }
 
 KD_API void KD_APIENTRY kdLogMessage(const KDchar *string)
 {
+    size_t len = strlen(string);
+    if (len <= 0) {
+        return;
+    }
+
+    char* msg = 0;
+    if (strcmp(&string[len - 1], "\n")) {
+        msg = (char *)malloc(len + 2);
+        strcpy(msg, string);
+        strcat(msg, "\n");
+    } else {
+        msg = (char *)string;
+    }
+
+    fprintf(stdout, "%s", msg);
+
+    if (msg != string) {
+        free(msg);
+    }
 }
